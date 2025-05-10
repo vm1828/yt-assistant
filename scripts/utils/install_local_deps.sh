e46141fc1c85ea473becf0592782848be9632f30
vm1828 <vm18281828@gmail.com>
2025-05-10 14:47:34 +0300

    chore(deps): add deps for psycopg2

--- scripts/utils/install_local_deps.sh
+++ scripts/utils/install_local_deps.sh
@@ -2,6 +2,9 @@
 
 echo "Installing Python dependencies..."
 cd yt_assistant_api
+sudo apt-get update
+sudo apt-get install -y libpq-dev build-essential python3-dev
+sudo ln -s /usr/bin/python3 /usr/bin/python
 poetry install --no-root
 cd ..
 
