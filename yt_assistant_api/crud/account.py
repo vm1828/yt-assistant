c5252b9fb0698c57c17dcdb49a20953be9ca91d1
vm1828 <vm18281828@gmail.com>
2025-05-24 10:25:07 +0300

    chore(api): make /accounts/ async, update unit tests

--- yt_assistant_api/crud/account.py
+++ yt_assistant_api/crud/account.py
@@ -5,21 +5,21 @@ from models.account import Account
 from schemas.account import AccountCreate
 
 
-def create_account(db: Session, data: AccountCreate):
+async def create_account(db: AsyncSession, data: AccountCreate):
     account = Account(id=data.id)
     db.add(account)
-    db.commit()
-    db.refresh(account)
+    await db.commit()
+    await db.refresh(account)
     return account
-
-
-def get_account_by_id_sync(db: Session, account_id: str):
-    stmt = select(Account).where(Account.id == account_id)
-    result = db.execute(stmt)
-    return result.scalar_one_or_none()
 
 
 async def get_account_by_id_async(db: AsyncSession, account_id: str):
     stmt = select(Account).where(Account.id == account_id)
     result = await db.execute(stmt)
+    return result.scalar_one_or_none()
+
+
+def get_account_by_id_sync(db: Session, account_id: str):
+    stmt = select(Account).where(Account.id == account_id)
+    result = db.execute(stmt)
     return result.scalar_one_or_none()
