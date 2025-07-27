d3654bd75f50888c016d4e40ca9e8359565975bb
vm18281828 <vm18281828@gmail.com>
2025-07-27 20:26:32 +0200

    chore(deps): update youtube-transcript-api version to 1.2.1

--- scripts/utils/install_local_deps.sh
+++ scripts/utils/install_local_deps.sh
@@ -3,10 +3,13 @@
 echo "Installing Python dependencies..."
 cd yt_assistant_api
 sudo apt-get update
-sudo apt-get install -y libpq-dev build-essential python3-dev
+sudo apt-get install -y libpq-dev build-essential python3-dev python3-poetry
 sudo ln -s /usr/bin/python3 /usr/bin/python
 poetry install --with dev --no-interaction
 cd ..
 
 echo "Instaling Node.js dependencies..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash # Download and install nvm
+\. "$HOME/.nvm/nvm.sh"                                                          # in lieu of restarting the shell
+nvm install 22
 npm install --prefix yt_assistant_client/
