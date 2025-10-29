f9417b82977641384c89149db21d54d7f751b4bf
vm1828 <vm18281828@gmail.com>
2025-10-29 10:58:51 +0100

    feat(emb): use uv package manager for emb service

--- scripts/utils/install_local_deps.sh
+++ scripts/utils/install_local_deps.sh
@@ -1,11 +1,22 @@
 # Install local dependencies
 
-echo "Installing Python dependencies..."
+echo "Installing Python dependencies for yt_assistant_api..."
 cd yt_assistant_api
 sudo apt-get update
 sudo apt-get install -y libpq-dev build-essential python3-dev python3-poetry
 sudo ln -s /usr/bin/python3 /usr/bin/python
 poetry install --with dev --no-interaction
+cd ..
+
+echo "Installing Python dependencies for yt_assistant_emb..."
+cd yt_assistant_emb
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv sync
+# Manually install torch without GPU
+source .venv/bin/activate
+pip install --break-system-packages torch==2.9.0+cpu --index-url https://download.pytorch.org/whl/cpu
+pip install --break-system-packages sentence-transformers==5.1.2
+deactivate
 cd ..
 
 echo "Instaling Node.js dependencies..."
